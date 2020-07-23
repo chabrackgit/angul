fdbc415905af308902917e856d0bc19417ce6bd4
mohamed SABER <mohamedsaber@MacBook-de-mohamed.local>
2020-07-23 23:51:14 +0200

    string interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Formation Angular 8';
+  nombre = 37;
+  bool = true;
+  obj = {
+    cle: 'valeur'
+  };
+
+  public square(nbr: number): number {
+    return nbr*nbr;
+  }
 }
